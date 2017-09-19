4c8ff3cf6e5cbb6416209f719f4495b6bf3e1e66
wencaizhang <1052642137@qq.com>
2017-09-19 22:17:45 +0800

    update

--- scripts/zFormHint.js
+++ scripts/zFormHint.js
@@ -1,84 +1,100 @@
 ;
 (function ($) {
     var pluginName = "formHint",
-    defaults = {
-        arrow: "top",
-        message: "xxxxxx"
-    }
+    defaults = {}
 
     function FormHint (ele, options) {
         var self = this;
-        self.ele = ele;
-        $(self.ele).attr("data-msg")
-        self.settings = $.extend(defaults, { message: $(self.ele).attr("data-msg") }, options);
+        self.ele = $(ele);
+        self.ele.attr("data-msg");
+        self.settings = $.extend(defaults, { message: self.ele.attr("data-msg") }, options);
         self.init();
     }
     FormHint.prototype = {
 
         //提示信息定位，传入参数（提示对象，提示信息内容）
         init: function (configObj) {
-
-            var $ele = $(this);
-            this.render();
-
-            //点击后提示隐藏
-            $ele.focus(function () {
-                $tip.fadeIn('slow');
-            });
-            $ele.blur(function () {
-                // $tip.fadeOut('slow');
-            });
+            var self = this;
+            self.show();
+            self.eventsHandler();
+            self.computePosition();
         },
 
-        computePosition: function ($ele, $tip, $arrow) {
-            var objOffset = $ele.offset();
-            var objWidth = $ele.outerWidth();
-            var tipWidth = $tip.outerWidth();
-            var objHeight = $ele.outerHeight();
-            var tipHeight = $tip.outerHeight();
-            var hintTop,
-                hintLeft;
+        computePosition: function () {
+            var self = this;
+
+            var objOffset = self.ele.offset();
+            var objWidth  = self.ele.outerWidth();
+            var objHeight = self.ele.outerHeight();
+
+            var tipWidth  = self.tip.outerWidth();
+            var tipHeight = self.tip.outerHeight();
+
+            var hintTop, hintLeft;
 
             // 计算位置
-            switch (this.settings.arrow) {
+            switch (self.settings.arrow) {
                 case "top":
-                    $arrow.addClass("arrow-top");
                     hintLeft = objOffset.left + (objWidth - tipWidth) / 2;
-                    hintTop = objOffset.top - tipHeight - 15;
+                    hintTop  = objOffset.top  - tipHeight - 15;
                     break;
                 case "left":
-                    $arrow.addClass("arrow-left");
                     hintLeft = objOffset.left - tipWidth - 15;
-                    hintTop = objOffset.top + (objHeight - tipHeight) / 2;
+                    hintTop  = objOffset.top  + (objHeight - tipHeight) / 2;
                     break;
                 case "right":
-                    $arrow.addClass("arrow-right");
                     hintLeft = objOffset.left + objWidth + 15;
-                    hintTop = objOffset.top + (objHeight - tipHeight) / 2;
+                    hintTop  = objOffset.top  + (objHeight - tipHeight) / 2;
                     break;
                 case "bottom":
-                    $arrow.addClass("arrow-bottom");
                     hintLeft = objOffset.left + (objWidth - tipWidth) / 2;
-                    hintTop = objOffset.top + objHeight + 15;
+                    hintTop  = objOffset.top  + objHeight + 15;
                     break;
                 default:
                     break;
             }
 
-            return {top: hintTop, left: hintLeft};
+            self.tip.offset({ top: hintTop, left: hintLeft });
         },
 
-        render: function () {
+        _createTip: function () {
             var self = this;
+            var className = "arrow-" + this.settings.arrow;
+            console.log(this.settings.arrow);
+            console.log(className);
+
             //表单验证提示html
             var $tip = $("<div class='form-hint'><div class='form-hint-text'></div><div class='form-hint-arrow'></div></div>");
             var $text = $tip.find(".form-hint-text");
             var $arrow = $tip.find(".form-hint-arrow");
-            
+            $arrow.addClass(className);
             $text.text(self.settings.message);
-            $("body").append($tip);
-            var position = self.computePosition($(self.ele), $tip, $arrow);
-            $tip.offset(position);
+            
+            self.tip = $tip;
+            self.tip.css('display', 'none');
+        },
+
+        show: function () {
+            var self = this;
+            self._createTip();
+            $("body").append(self.tip);
+        },
+        
+        eventsHandler: function () {
+            var self = this;
+            if (self.ele[0].tagName === "INPUT") {
+                var event = "focus", unevent = "blur";
+            } else {
+                var event = "mouseenter", unevent = "mouseleave";
+            }
+
+            self.ele.on(event, function () {
+                // self.computePosition();
+                self.tip.fadeIn('slow');
+            });
+            self.ele.on(unevent, function () {
+                self.tip['fadeOut']('slow');
+            });
         }
 
     }
